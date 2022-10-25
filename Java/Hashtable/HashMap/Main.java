169af34d1ba8a3d3f5461ab84ef187bf950780fb
Isaac1309 <carlos.isaacalvg@gmail.com>
2022-10-25 16:59:17 -0500

    Working with a HashMap using a class like a Key

--- Java/Hashtable/HashMap/Main.java
+++ Java/Hashtable/HashMap/Main.java
@@ -1,5 +1,5 @@
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,6 +22,17 @@ public class Main {
         System.out.println("\nUsing remove: ");
         map.remove(2);
         for(Map.Entry<Integer, String> entry : map.entrySet())
+            System.out.println(entry.getKey()+" "+entry.getValue());
+
+        System.out.println("\n\nUsing a class 'Person' like a Key in a HashMap: ");
+        Map<Person, String> mapPerson = new HashMap<>();
+        Person person = new Person("John", 32);
+
+        mapPerson.put(new Person("Juan", 16),"Perez");
+        mapPerson.put(person,"Doe");
+
+        System.out.println("\nUsing Entry: ");
+        for(Map.Entry<Person, String> entry : mapPerson.entrySet())
             System.out.println(entry.getKey()+" "+entry.getValue());
     }
 }
