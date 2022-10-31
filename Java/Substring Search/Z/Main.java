9cca44c1dd7107597274c2fa6e01637f6424bc20
Isaac1309 <carlos.isaacalvg@gmail.com>
2022-10-31 13:41:39 -0600

    Working with Z algorithm

---
+++ Java/Substring Search/Z/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ZAlgorithm search = new ZAlgorithm("This is a test", "test");
+        //We obtain the position where the string meets the search condition (counting the spaces)
+        search.search();
+    }
+}
